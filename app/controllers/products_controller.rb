f7fd3c6cbaa6f70194cc1c8716cc3d045bbdf001
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-16 14:47:06 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,21 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
+  end
+
+  def create
+  end 
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
+  end
+
 end
